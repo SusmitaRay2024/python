38ff4b96c3c978614dd096437a1c519e205c6c6a
SusmitaRay2023 <122145024+SusmitaRay2023@users.noreply.github.com>
2024-12-21 17:38:09 +0530

    Add new file and modify arithmaticOP file

--- ArithmaticOP.py
+++ ArithmaticOP.py
@@ -2,17 +2,20 @@
 # (+) will do both Addition & Concatenation
 
 #Addition
+print("Addition two number")
 a = int(input("Enter First Number "))
 b = int(input("Enter Second Number "))
 c = a+b
 print("Addition is: ",c)
  
 #Concatenation
+print("Concatenation two number")
 d = input("Enter First No ")
 e = input("Enter Second No ")
 print(d+e)
 
 #(-) use for substraction
+print("Substraction two number")
 f = int(input("Enter First Number "))
 g = int(input("Enter Second Number "))
 h = f-g
@@ -20,6 +23,7 @@ print("Substraction is: ",h)
 
 #(*) will do more things like-
 #Multiplication
+print("Multiplication two number")
 j = int(input("Enter First Number "))
 k = int(input("Enter Second Number "))
 l = j*k
@@ -27,18 +31,23 @@ print("Multiplication is: ",l)
 
 #(/) use for Division
 #always get the float value(7.5)
+print("print Division of two number")
 print(15/2)
 
-#Print Multiple Line
+#Print a Word in Multiple Line
+print("Print a Word in Multiple Line")
 print("Hello"*3)
 
 #(**) use for to calculate power of number
+print("print power of numbers")
 print(5**3)
 print(5**2)
 
 #(%) Modulus operator to get the remainder 
+print("print remainder")
 print(15%2) #o/p-1
 
 #(//) Floor Division Operator
 #To get the integer value from float value
+print("Get integer value using floor dividion operator")
 print(15//2) #o/p-7
