cc0998b45f578bf40d4d5617d4ecddb09eb8c680
SusmitaRay2023 <122145024+SusmitaRay2023@users.noreply.github.com>
2024-12-23 10:02:03 +0530

    new files

---
+++ table.py
@@ -0,0 +1,6 @@
+# Write a program to print table of a number
+n=int(input("Enter a Number"))
+i=1
+while(i<=n):
+    print(n*i)
+    i+=1
---
+++ wordformatCo.py
@@ -0,0 +1,33 @@
+n = int(input("Enter a Number: "))
+s = 0
+
+# Reverse the number
+while n != 0:
+    r = n % 10  # Get the last digit
+    s = s * 10 + r  # Reverse the digits
+    n = n // 10  # Remove the last digit from n
+
+# Iterate through the reversed number
+while s != 0:
+    r = s % 10  # Extract each digit from the reversed number
+    if r == 0:
+        print("Zero")
+    elif r == 1:
+        print("One")
+    elif r == 2:
+        print("Two")
+    elif r == 3:
+        print("Three")
+    elif r == 4:
+        print("Four")
+    elif r == 5:
+        print("Five")
+    elif r == 6:
+        print("Six")
+    elif r == 7:
+        print("Seven")
+    elif r == 8:
+        print("Eight")
+    elif r == 9:
+        print("Nine")
+    s = s // 10  # Move to the next digit
